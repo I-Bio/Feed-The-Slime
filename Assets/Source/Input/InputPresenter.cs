87e81e918963c746ff44b190c0862b3ac8f30fa7
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-03-13 20:35:54 +0300

    Вынес скрипты ввода в папку и namespace

---
+++ Assets/Source/Input/InputPresenter.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Input
+{
+    public class InputPresenter
+    {
+        private readonly PlayerInput _model;
+        private readonly Joystick _joystick;
+        private readonly Mover _player;
+
+        public InputPresenter(PlayerInput model, Joystick joystick, Mover player)
+        {
+            _model = model;
+            _joystick = joystick;
+            _player = player;
+        }
+
+        public void Enable()
+        {
+            _model.Player.Move.performed += OnMove;
+            _model.Player.Touch.performed += OnTouch;
+            _joystick.Released += OnJoystickReleased;
+
+            _model.Enable();
+        }
+
+        public void Disable()
+        {
+            _model.Player.Move.performed -= OnMove;
+            _model.Player.Touch.performed -= OnTouch;
+            _joystick.Released -= OnJoystickReleased;
+
+            _model.Disable();
+        }
+
+        private void OnMove(InputAction.CallbackContext context)
+        {
+            Vector2 input = context.ReadValue<Vector2>();
+            _player.ReadInput(input);
+        }
+
+        private void OnTouch(InputAction.CallbackContext context)
+        {
+            Vector2 position = _model.Player.ScreenPosition.ReadValue<Vector2>();
+            
+            if (position == Vector2.zero)
+                return;
+            
+            _joystick.Activate(position);
+        }
+
+        private void OnJoystickReleased()
+        {
+            _player.ReadInput(Vector2.zero);
+        }
+    }
+}
---
+++ Assets/Source/Input/Joystick.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Input
+{
+    public class Joystick : MonoBehaviour
+    {
+        [SerializeField] private Camera _camera;
+        [SerializeField] private RectTransform _parent;
+        [SerializeField] private RectTransform _knob;
+        [SerializeField] private RectTransform _holderTransform;
+        [SerializeField] private GameObject _holder;
+        
+        private Coroutine _coroutine;
+
+        public event Action Released;
+
+        private void Start()
+        {
+            Release();
+        }
+
+        public void Activate(Vector2 position)
+        {
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(_parent, position, _camera,
+                    out Vector2 localPosition) == false)
+                return;
+
+            _holderTransform.anchoredPosition = localPosition;
+            _holder.SetActive(true);
+
+            if (_coroutine != null)
+                StopCoroutine(_coroutine);
+            
+            _coroutine = StartCoroutine(ReleaseRoutine());
+        }
+
+        private void Release()
+        {
+            Released?.Invoke();
+            _holder.SetActive(false);
+        }
+
+        private IEnumerator ReleaseRoutine()
+        {
+            Vector2 lastPosition = Vector2.zero;
+
+            while (_knob.anchoredPosition != Vector2.zero || lastPosition == Vector2.zero)
+            {
+                lastPosition = _knob.anchoredPosition;
+                yield return null;
+            }
+            
+            Release();
+        }
+    }
+}
