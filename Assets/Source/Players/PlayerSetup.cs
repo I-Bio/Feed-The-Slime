3bd7e11a73b5d5b314690987d45d6b56254a801b
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-03-19 22:44:22 +0300

    установка новых зависимостей в установщике игрока

--- Assets/Source/Players/PlayerSetup.cs
+++ Assets/Source/Players/PlayerSetup.cs
@@ -1,51 +1,94 @@
-﻿using UnityEngine;
+﻿using Boosters;
+using UnityEngine;
 
 namespace Players
 {
     [RequireComponent(typeof(PlayerCollisionDetector))]
     [RequireComponent(typeof(PlayerScanner))]
     [RequireComponent(typeof(SizeScaler))]
+    [RequireComponent(typeof(Mover))]
+    [RequireComponent(typeof(BoosterVisualizer))]
     public class PlayerSetup : MonoBehaviour
     {
+        private const float Zero = 0f;
+        
+        [Header("UI")] 
         [SerializeField] private LevelBar _levelBar;
         [SerializeField] private StageBar _stageBar;
+
+        [Space, Header("Player Options")] 
         [SerializeField] private float _scaleFactor;
+
         [SerializeField] private float _scoreScaler;
         [SerializeField] private float _startMaxScore;
         [SerializeField] private int _levelsPerStage;
         [SerializeField] private SatietyStage _startStage;
-        
+
+        [Space, Header("Move Options")] 
+        [SerializeField] private Transform _rotationPoint;
+        [SerializeField] private float _startSpeed;
+
+        [Space, Header("Booster Effects")] 
+        [SerializeField] private float _updateDelay;
+        [SerializeField] private ParticleSystem _speedEffect;
+        [SerializeField] private ParticleSystem _scoreEffect;
+
         private PlayerCollisionDetector _collisionDetector;
         private PlayerScanner _scanner;
         private SizeScaler _sizeScaler;
+        private Mover _mover;
         private Transform _transform;
 
         private Goop _model;
-        private PlayerPresenter _presenter;
-        
+        private BoosterInjector _injector;
+        private BoosterEjector _ejector;
+        private BoosterService _service;
+        private BoosterVisualizer _effectsVisualizer;
+
+        private PlayerPresenter _playerPresenter;
+        private BoosterPresenter _boosterPresenter;
+
         private void Awake()
         {
             _collisionDetector = GetComponent<PlayerCollisionDetector>();
             _scanner = GetComponent<PlayerScanner>();
             _sizeScaler = GetComponent<SizeScaler>();
+            _mover = GetComponent<Mover>();
             _transform = transform;
 
+            IMovable movable = new Speed(_startSpeed, Zero);
+            ICalculableScore calculableScore = new Score(Zero);
+
             _model = new Goop(_startStage, _scoreScaler, _startMaxScore, _levelsPerStage);
-            _presenter = new PlayerPresenter(_model, _collisionDetector, _scanner, _sizeScaler, _levelBar, _stageBar);
-            
+            _injector = new BoosterInjector();
+            _ejector = new BoosterEjector(calculableScore, movable);
+            _service = new BoosterService();
+
+            _playerPresenter = new PlayerPresenter(_model, _collisionDetector, _scanner, _sizeScaler, _levelBar,
+                _stageBar, _service);
+            _boosterPresenter = new BoosterPresenter(_model, _mover, _injector, _ejector, _service, _effectsVisualizer);
+
             _scanner.SetSatiety(_startStage);
             _sizeScaler.Initialize(_transform, _scaleFactor);
-            _model.IncreaseScore(0f);
+            _mover.Initialize(movable, _rotationPoint, _transform.forward);
+            _effectsVisualizer.Initialize(_updateDelay, _speedEffect, _scoreEffect);
         }
 
         private void OnEnable()
         {
-            _presenter.Enable();
+            _playerPresenter.Enable();
+            _boosterPresenter.Enable();
+        }
+
+        private void Start()
+        {
+            _model.IncreaseScore(Zero);
         }
 
         private void OnDisable()
         {
-            _presenter.Disable();
+            _playerPresenter.Disable();
+            _boosterPresenter.Enable();
         }
     }
 }
