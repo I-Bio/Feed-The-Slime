1c640d3a8afa0491beeab0c9de6a36c9bfaefaeb
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-03-20 13:46:19 +0300

    Создан загрузчик уровня

---
+++ Assets/Source/Spawners/LevelLoader.cs
@@ -0,0 +1,25 @@
+﻿using Boosters;
+using Players;
+using UnityEngine;
+
+namespace Spawners
+{
+    public class LevelLoader : MonoBehaviour
+    {
+        [SerializeField] private BoosterSpawner _boosterSpawner;
+        [SerializeField] private FoodSpawner _foodSpawner;
+        [SerializeField] private PlayerSetup _player;
+        [SerializeField] private Transform _playerPoint;
+        [SerializeField] private float _startSpeed;
+        
+        private void Awake()
+        {
+            IMovable movable = new Speed(_startSpeed);
+            ICalculableScore calculableScore = new Score();
+
+            _foodSpawner.Initialize();
+            _boosterSpawner.Initialize(movable, calculableScore);
+            Instantiate(_player, _playerPoint.position, Quaternion.identity).Initialize(movable, calculableScore);
+        }
+    }
+}
