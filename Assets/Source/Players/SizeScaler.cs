525d30d00fcc5a6906fb2bc370893a81eddf053d
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-03-22 20:41:40 +0300

    Изменение формулы увеличения в размерах

--- Assets/Source/Players/SizeScaler.cs
+++ Assets/Source/Players/SizeScaler.cs
@@ -6,16 +6,18 @@ namespace Players
     {
         private Transform _transform;
         private float _scaleFactor;
+        private Vector3 _startScale;
 
         public void Initialize(Transform transform, float scaleFactor)
         {
             _transform = transform;
             _scaleFactor = scaleFactor;
+            _startScale = _transform.localScale;
         }
 
         public void Scale(SatietyStage stage)
         {
-            _transform.localScale *= _scaleFactor * (int)stage;
+            _transform.localScale = _startScale * (_scaleFactor * (int)stage);
         }
     }
 }
