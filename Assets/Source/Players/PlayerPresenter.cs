b99f7ee04b2980ef3d9e3f0f432f86a915c9211c
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-03-22 18:51:04 +0300

    Смена уровня поиска у сканеров

--- Assets/Source/Players/PlayerPresenter.cs
+++ Assets/Source/Players/PlayerPresenter.cs
@@ -58,6 +58,7 @@ namespace Players
 
         private void OnSizeIncreased(SatietyStage stage)
         {
+            _scanner.SetSatiety(stage);
             _sizeScaler.Scale(stage);
             _scanner.Rescan();
         }
