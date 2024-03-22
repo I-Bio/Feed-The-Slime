271632424733beee23c48264dafd6d752b27e96e
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-03-22 18:56:15 +0300

    Изменение загрузки уровня

--- Assets/Source/Spawners/LevelLoader.cs
+++ Assets/Source/Spawners/LevelLoader.cs
@@ -1,4 +1,5 @@
-﻿using Boosters;
+﻿using System;
+using Boosters;
 using Players;
 using UnityEngine;
 
@@ -7,19 +8,21 @@ namespace Spawners
     public class LevelLoader : MonoBehaviour
     {
         [SerializeField] private BoosterSpawner _boosterSpawner;
-        [SerializeField] private FoodSpawner _foodSpawner;
         [SerializeField] private PlayerSetup _player;
-        [SerializeField] private Transform _playerPoint;
         [SerializeField] private float _startSpeed;
-        
+        [SerializeField] private Transform[] _zonePoints;
+        [SerializeField] private FoodPreparer[] _zoneTemplates;
+
         private void Awake()
         {
             IMovable movable = new Speed(_startSpeed);
             ICalculableScore calculableScore = new Score();
 
-            _foodSpawner.Initialize();
+            _player.Initialize(movable, calculableScore);
             _boosterSpawner.Initialize(movable, calculableScore);
-            Instantiate(_player, _playerPoint.position, Quaternion.identity).Initialize(movable, calculableScore);
+            
+            foreach (Transform zonePoint in _zonePoints)
+                Instantiate(_zoneTemplates.GetRandom(), zonePoint).Initialize();
         }
     }
 }
