e2016d0b74c723b4f4d19110010a87b49b4540e6
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-03-22 14:28:33 +0300

    Спавнер еды изменен на подготовитель

---
+++ Assets/Source/Spawners/FoodPreparer.cs
@@ -0,0 +1,16 @@
+﻿using Foods;
+using UnityEngine;
+
+namespace Spawners
+{
+    public class FoodPreparer : SpawnableObject
+    {
+        [SerializeField] private FoodSetup[] _foods;
+
+        public void Initialize()
+        {
+            foreach (FoodSetup food in _foods)
+                food.Initialize();
+        }
+    }
+}
