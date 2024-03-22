696779b0697197ba21775086bae59000c34e5e05
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-03-22 18:55:03 +0300

    Префаб бустреа

---
+++ Assets/Source/Boosters/Templates/Booster.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Spawners;
+using TMPro;
+using UnityEngine;
+
+namespace Boosters
+{
+    public class Booster : SpawnableObject, IBooster
+    {
+        [SerializeField] private TextMeshPro _text;
+        [SerializeField] private SpriteRenderer _renderer;
+        
+        private IStatBuffer _boost;
+
+        public void Initialize(KeyValuePair<IStatBuffer, KeyValuePair<string, Sprite>> boostValues)
+        {
+            _boost = boostValues.Key;
+            _text.SetText(boostValues.Value.Key);
+            _renderer.sprite = boostValues.Value.Value;
+        }
+
+        public void Use()
+        {
+            Push();
+        }
+        
+        public IStatBuffer GetBoost()
+        {
+            return _boost; 
+        }
+    }
+}
