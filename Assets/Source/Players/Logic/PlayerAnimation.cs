4ad5760e091ef508f5b3c609846344475f1ad4dc
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-03-27 21:47:34 +0300

    Создан скрипт анимаций

---
+++ Assets/Source/Players/Logic/PlayerAnimation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Players
+{
+    public class PlayerAnimation : MonoBehaviour
+    {
+        private Animator _animator;
+        
+        private string _eat;
+        private string _idle;
+        private string _hide;
+
+        public void Initialize(Animator animator, string eat, string idle, string hide)
+        {
+            _animator = animator;
+            _eat = eat;
+            _idle = idle;
+            _hide = hide;
+        }
+
+        public void PlayAttack()
+        {
+            _animator.SetTrigger(_eat);
+        }
+
+        public void PlayIdle()
+        {
+            _animator.SetTrigger(_idle);
+        }
+
+        public void PlayHide()
+        {
+            _animator.SetTrigger(_hide);
+        }
+    }
+}
