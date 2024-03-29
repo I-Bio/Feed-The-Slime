17fa3e8842b7b1c23521398870d4cac4fbc21fa8
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-03-29 18:47:57 +0300

    Перенесено в папку. Дописан класс.

---
+++ Assets/Source/Enemies/Logic/EnemyAnimation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Enemies
+{
+    public class EnemyAnimation : MonoBehaviour
+    {
+        private Animator _animator;
+        
+        private string _move;
+        private string _idle;
+        private bool _isMoved;
+
+        public void Initialize(Animator animator, string move, string idle)
+        {
+            _animator = animator;
+            _move = move;
+            _idle = idle;
+        }
+
+        public void PlayMove()
+        {
+            if (_isMoved == true)
+                return;
+
+            _isMoved = true;
+            _animator.SetTrigger(_move);
+        }
+
+        public void PlayIdle()
+        {
+            _isMoved = false;
+            _animator.SetTrigger(_idle);
+        }
+    }
+}
