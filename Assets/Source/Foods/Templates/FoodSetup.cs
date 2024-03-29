0fa831866833303e972910fc8919775723a1b667
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-03-29 18:41:56 +0300

    Сменён MeshRenderer на Renderer

--- Assets/Source/Foods/Templates/FoodSetup.cs
+++ Assets/Source/Foods/Templates/FoodSetup.cs
@@ -4,32 +4,32 @@ namespace Foods
 {
     [RequireComponent(typeof(EdiblePart))]
     [RequireComponent(typeof(ObjectHighlighter))]
-    [RequireComponent(typeof(MeshRenderer))]
     public class FoodSetup : MonoBehaviour
     {
         [SerializeField] private Material[] _standard;
         [SerializeField] private Material[] _highlighted;
         [SerializeField] private float _scorePerEat;
         [SerializeField] private SatietyStage _stage;
+        [SerializeField] private Renderer _renderer;
 
         private EdiblePart _ediblePart;
         private ObjectHighlighter _highlighter;
-        private MeshRenderer _meshRenderer;
             
         private Food _model;
         private FoodPresenter _presenter;
+
+        public SatietyStage Stage => _stage;
         
         public void Initialize()
         {
             _ediblePart = GetComponent<EdiblePart>();
             _highlighter = GetComponent<ObjectHighlighter>();
-            _meshRenderer = GetComponent<MeshRenderer>();
             
             _model = new Food(_stage);
             _presenter = new FoodPresenter(_model, _ediblePart, _highlighter);
             
             _ediblePart.Initialize(_scorePerEat);
-            _highlighter.Initialize(_meshRenderer, _standard, _highlighted);
+            _highlighter.Initialize(_renderer, _standard, _highlighted);
             
             _presenter.Enable();
         }
