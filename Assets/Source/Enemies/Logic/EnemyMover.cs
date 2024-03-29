406612573b4d167c590f5ad7e9c772bbf5843916
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-03-29 18:48:36 +0300

    Перенесено в папку. Изменена инициализация.

--- Assets/Source/Enemies/Logic/EnemyMover.cs
+++ Assets/Source/Enemies/Logic/EnemyMover.cs
@@ -8,16 +8,17 @@ namespace Enemies
     [RequireComponent(typeof(NavMeshAgent))]
     public class EnemyMover : MonoBehaviour
     {
-        [SerializeField] private float _delay;
-        
         private NavMeshAgent _agent;
         private Vector3 _currentDestination;
+        private float _delay;
 
         public event Action GoingMove;
-        
-        public void Initialize()
+
+        public void Initialize(float delay)
         {
             _agent = GetComponent<NavMeshAgent>();
+            _delay = delay;
+            StartCoroutine(MoveRoutine());
         }
 
         public void Move(Vector3 position)
