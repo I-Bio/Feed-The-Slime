de38bc2c41fa2f044df19faf2effcf687d85fb1d
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-03-29 18:49:04 +0300

    Перенесено в папку. Добавлено сообщение с анимациями

--- Assets/Source/Enemies/Logic/EnemyPresenter.cs
+++ Assets/Source/Enemies/Logic/EnemyPresenter.cs
@@ -6,11 +6,13 @@ namespace Enemies
     {
         private readonly Enemy _model;
         private readonly EnemyMover _mover;
+        private readonly EnemyAnimation _animation;
         
-        public EnemyPresenter(Enemy model, EnemyMover mover)
+        public EnemyPresenter(Enemy model, EnemyMover mover, EnemyAnimation animation)
         {
             _model = model;
             _mover = mover;
+            _animation = animation;
         }
         
         public void Enable()
@@ -31,12 +33,13 @@ namespace Enemies
 
         private void OnMoved(Vector3 position)
         {
+            _animation.PlayMove();
             _mover.Move(position);
         }
 
         private void OnIdled()
         {
-            
+            _animation.PlayIdle();
         }
 
         private void OnGoingMove()
