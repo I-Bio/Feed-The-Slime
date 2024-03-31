251555478122168e5accfa544e98650bf63afefb
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-03-31 20:05:12 +0300

    Добавлено взаимодействие при контакте

--- Assets/Source/Players/Abilities/Projectile.cs
+++ Assets/Source/Players/Abilities/Projectile.cs
@@ -1,27 +1,34 @@
-﻿using System;
+﻿using Foods;
 using Spawners;
 using UnityEngine;
 
-namespace Abilities
+namespace Players
 {
     [RequireComponent(typeof(Rigidbody))]
     public class Projectile : SpawnableObject
     {
+        [SerializeField] private FoodSetup _template;
+
+        private EatableSpawner _spawner;
         private Rigidbody _rigidbody;
-        
-        private void Awake()
-        {
-            _rigidbody = GetComponent<Rigidbody>();
-        }
 
         private void OnCollisionEnter(Collision collision)
         {
+            if (collision.collider.TryGetComponent(out EdiblePart food))
+            {
+                food.Allow();
+                food.TryEat(out float score);
+                _spawner.Spawn(collision.transform.position, score);
+            }
+            
             _rigidbody.velocity = Vector3.zero;
             Push();
         }
 
-        public void Initialize(Vector3 velocity)
+        public void Initialize(Vector3 velocity, EatableSpawner spawner)
         {
+            _spawner = spawner;
+            _rigidbody = GetComponent<Rigidbody>();
             _rigidbody.velocity = velocity;
         }
     }
