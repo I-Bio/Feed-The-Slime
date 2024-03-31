17e6f969397358fea174021fae7bcaf5154c121e
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-03-31 16:03:51 +0300

    Создан снаряд плевка слизи

---
+++ Assets/Source/Players/Abilities/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Spawners;
+using UnityEngine;
+
+namespace Abilities
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class Projectile : SpawnableObject
+    {
+        private Rigidbody _rigidbody;
+        
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            _rigidbody.velocity = Vector3.zero;
+            Push();
+        }
+
+        public void Initialize(Vector3 velocity)
+        {
+            _rigidbody.velocity = velocity;
+        }
+    }
+}
