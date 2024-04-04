d14b13a789d304f2d6265202e4641a1e9cd8dae0
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-04-04 17:20:34 +0300

    Создана структура сериализованной пары

---
+++ Assets/Source/Global/Pair.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public struct SerializedPair<T>
+{
+    public int Key;
+    public T Value;
+        
+    public static bool operator ==(SerializedPair<T> first, SerializedPair<T> second) 
+    {
+        return first.Equals(second);
+    }
+
+    public static bool operator !=(SerializedPair<T> first, SerializedPair<T> second) 
+    {
+        return !first.Equals(second);
+    }
+    
+    public bool Equals(SerializedPair<T> other)
+    {
+        return Key == other.Key && EqualityComparer<T>.Default.Equals(Value, other.Value);
+    }
+
+    public override bool Equals(object @object)
+    {
+        return @object is SerializedPair<T> other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Key, Value);
+    }
+}
