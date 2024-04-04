c25e4f0ea4c5f2812a132d56f12b23f358551022
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-04-04 17:21:14 +0300

    Скрипт авторизации

---
+++ Assets/Source/Global/SceneNames.cs
@@ -0,0 +1,6 @@
+﻿public enum SceneNames
+{
+    Load,
+    Menu,
+    Game,
+}
---
+++ Assets/Source/Menu/Authorizer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using Agava.YandexGames;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Menu
+{
+    public class Authorizer : MonoBehaviour
+    {
+        private void Awake()
+        {
+            YandexGamesSdk.CallbackLogging = true;
+        }
+
+        private IEnumerator Start()
+        {
+            yield return YandexGamesSdk.Initialize(OnInitialize);
+        }
+
+        private void OnInitialize()
+        {
+            SceneManager.LoadScene((int)SceneNames.Menu);
+        }
+    }
+}
