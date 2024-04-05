fff20a50fb002d76b4d495ab48d0bf11b7e5b9dc
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-04-05 16:16:56 +0300

    убраны лишние поля

--- Assets/Source/Menu/Progress/PlayerCharacteristics.cs
+++ Assets/Source/Menu/Progress/PlayerCharacteristics.cs
@@ -6,18 +6,11 @@ namespace Menu
     [Serializable]
     public class PlayerCharacteristics : IReadOnlyCharacteristics
     {
-        [SerializeField] private float _speed;
-        [SerializeField] private float _scorePerEat;
-        [SerializeField] private int _lifeCount;
-        [SerializeField] private int _crystalsCount;
-        [SerializeField] private int _completedLevels;
-        [SerializeField] private bool _spitObtained;
-
-        public float Speed { get => _speed; set => _speed = value; }
-        public float ScorePerEat { get => _scorePerEat; set => _scorePerEat = value; }
-        public int LifeCount { get => _lifeCount; set => _lifeCount = value; }
-        public int CrystalsCount { get => _crystalsCount; set => _crystalsCount = value; }
-        public int CompletedLevels { get => _completedLevels; set => _completedLevels = value; }
-        public bool SpitObtained { get => _spitObtained; set => _spitObtained = value; }
+        [field: SerializeField] public float Speed { get; set; }
+        [field: SerializeField] public float ScorePerEat { get; set; }
+        [field: SerializeField] public int LifeCount { get; set; }
+        [field: SerializeField] public int CrystalsCount { get; set; }
+        [field: SerializeField] public int CompletedLevels { get; set; }
+        [field: SerializeField] public bool SpitObtained { get; set; }
     }
 }
