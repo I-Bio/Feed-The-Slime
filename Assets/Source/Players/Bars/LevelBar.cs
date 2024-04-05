8f46be29c35f0c3c4077d9bf410c9305a5c2dee1
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-04-06 00:10:38 +0300

    Левел бар теперь создаёт всплывающие циферки

--- Assets/Source/Players/Bars/LevelBar.cs
+++ Assets/Source/Players/Bars/LevelBar.cs
@@ -1,26 +1,42 @@
-﻿using TMPro;
+﻿using Spawners;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Players
 {
-    public class LevelBar : MonoBehaviour
+    public class LevelBar : ObjectPool
     {
         [SerializeField] private Slider _slider;
-        [SerializeField] private TextMeshProUGUI _currentScore;
-        [SerializeField] private TextMeshProUGUI _maxScore;
+        [SerializeField] private Transform _popUpPoint;
+        [SerializeField] private Transform _container;
+        [SerializeField] private string _slash;
+        [SerializeField] private TextMeshProUGUI _score;
         [SerializeField] private TextMeshProUGUI _level;
 
-        public void SetScore(float score, float maxScore)
+        private Vector3 _popUpPosition;
+        
+        public void Initialize(float score, int maxScore)
         {
-            _slider.value = score / maxScore;
-            _currentScore.SetText(Mathf.FloorToInt(score).ToString());
-            _maxScore.SetText(Mathf.FloorToInt(maxScore).ToString());
+            ChangeScore(score, maxScore);
+            _popUpPosition = _popUpPoint.position;
+        }
+        
+        public void SetScore(float score, int maxScore, float value)
+        {
+            PullAndSetParent<PopUpText>(_popUpPosition, _container).Initialize(value);
+            ChangeScore(score, maxScore);
         }
 
         public void SetLevel(int levelPoint)
         {
             _level.SetText(levelPoint.ToString());
+        }
+
+        private void ChangeScore(float score, int maxScore)
+        {
+            _slider.value = score / maxScore;
+            _score.SetText($"{Mathf.FloorToInt(score)}{_slash}{maxScore}");
         }
     }
 }
---
+++ Assets/Source/Players/Bars/PopUpText.cs
@@ -0,0 +1,19 @@
+﻿using Spawners;
+using TMPro;
+using UnityEngine;
+
+namespace Players
+{
+    public class PopUpText : SpawnableObject
+    {
+        [SerializeField] private string _trigger;
+        [SerializeField] private TextMeshProUGUI _text;
+        [SerializeField] private Animator _animator;
+        
+        public void Initialize(float value)
+        {
+            _text.SetText(value.ToString());
+            _animator.Play(_trigger);
+        }
+    }
+}
