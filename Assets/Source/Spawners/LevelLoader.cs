1152934538a56fc41a169e79a089ea3a3d276a9d
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-04-10 01:48:07 +0300

    В загрузчик уровня добавлены темы по уровню сложности и центральная тема

--- Assets/Source/Spawners/LevelLoader.cs
+++ Assets/Source/Spawners/LevelLoader.cs
@@ -10,22 +10,39 @@ namespace Spawners
         [SerializeField] private BoosterSpawner _boosterSpawner;
         [SerializeField] private PlayerSetup _player;
         [SerializeField] private Game _game;
+        [SerializeField] private Transform _center;
+        [SerializeField] private Renderer _ground;
         [SerializeField] private Transform[] _zonePoints;
-        [SerializeField] private ThemePreparer[] _zoneTemplates;
+        [SerializeField] private SerializedPair<int, ThemePreparer[]>[] _zoneTemplates;
+        [SerializeField] private SerializedPair<int, CenterPreparer[]>[] _centerTemplates;
 
         private void Awake()
         {
-            ITransferService rewardService = TransferService.Instance;
-            IMovable movable = new Speed(rewardService.Characteristics.Speed);
+            ITransferService transferService = TransferService.Instance;
+            IMovable movable = new Speed(transferService.Characteristics.Speed);
             ICalculableScore calculableScore =
-                new AdditionalScore(new Score(), rewardService.Characteristics.ScorePerEat, 0f);
-            
-            _game.Initialize(rewardService, _player.transform);
+                new AdditionalScore(new Score(), transferService.Characteristics.ScorePerEat, 0f, null);
+            int id = 0;
+
+            _game.Initialize(transferService, _player.transform);
             _player.Initialize(movable, calculableScore, _game);
             _boosterSpawner.Initialize(movable, calculableScore);
 
+            for (int i = 0; i < _zoneTemplates.Length; i++)
+            {
+                if (_zoneTemplates[i].Key < transferService.Characteristics.CompletedLevels) 
+                    continue;
+                
+                id = i;
+                break;
+            }
+            
+            Instantiate(_centerTemplates[id].Value.GetRandom(), _center)
+                .Initialize(_player.GetComponent<IHidden>(), _ground);
+
             foreach (Transform zonePoint in _zonePoints)
-                Instantiate(_zoneTemplates.GetRandom(), zonePoint).Initialize(_player.GetComponent<IHidden>());
+                Instantiate(_zoneTemplates[id].Value.GetRandom(), zonePoint)
+                    .Initialize(_player.GetComponent<IHidden>());
         }
     }
 }
