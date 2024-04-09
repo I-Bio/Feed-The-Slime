2fde45022c2973ed637ec33de9f57f61b4724bff
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-04-10 01:41:01 +0300

    Добавлена обработка лидерборда у переключателя окон

--- Assets/Source/Menu/Windows/WindowSwitcher.cs
+++ Assets/Source/Menu/Windows/WindowSwitcher.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using Agava.YandexGames;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Menu
@@ -8,11 +10,13 @@ namespace Menu
     {
         [SerializeField] private Button _evolution;
         [SerializeField] private Button _leaders;
+        [SerializeField] private Button _authorize;
         [SerializeField] private Button[] _closeButtons;
-        [SerializeField] private HideOption _option;
 
         private Screen _screen;
 
+        public event Action LeaderboardOpened;
+        
         private void Awake()
         {
             _screen = GetComponent<Screen>();
@@ -21,27 +25,57 @@ namespace Menu
         private void OnEnable()
         {
             _evolution.onClick.AddListener(ShowEvolution);
+            _leaders.onClick.AddListener(ShowLeader);
+            _authorize.onClick.AddListener(Authorize);
             
             foreach (Button button in _closeButtons)
-                button.onClick.AddListener(Hide);
+                button.onClick.AddListener(ShowMain);
         }
 
         private void OnDisable()
         {
             _evolution.onClick.RemoveListener(ShowEvolution);
+            _leaders.onClick.RemoveListener(ShowLeader);
+            _authorize.onClick.RemoveListener(Authorize);
             
             foreach (Button button in _closeButtons)
-                button.onClick.RemoveListener(Hide);
+                button.onClick.RemoveListener(ShowMain);
         }
         
         public void Hide()
         {
-            _screen.HideWindow(_option);
+            _screen.Hide();
         }
 
+        public void ShowMain()
+        {
+            _screen.SetWindow((int)MainMenuWindows.Main);
+        }
+        
         private void ShowEvolution()
         {
             _screen.SetWindow((int)MainMenuWindows.Evolution);
+        }
+
+        private void ShowLeader()
+        {
+            if (PlayerAccount.IsAuthorized)
+            {
+                PlayerAccount.RequestPersonalProfileDataPermission(() =>
+                {
+                    LeaderboardOpened?.Invoke();
+                    _screen.SetWindow((int)MainMenuWindows.Leader);
+                });
+                return;
+            }
+
+            _screen.SetWindow((int)MainMenuWindows.Authorize);
+        }
+
+        private void Authorize()
+        {
+            PlayerAccount.Authorize();
+            ShowMain();
         }
     }
 }
