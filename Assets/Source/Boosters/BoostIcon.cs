14775128d809dee185c82bfb31a75524c4a1f139
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-04-10 01:06:41 +0300

    Создано отображение отката баффа

---
+++ Assets/Source/Boosters/BoostIcon.cs
@@ -0,0 +1,18 @@
+﻿using Players;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Boosters
+{
+    public class BoostIcon : AbilityButton
+    {
+        [SerializeField] private Image _image;
+        
+        public BoostIcon Initialize(float coolDown, Sprite sprite)
+        {
+            CoolDown = coolDown;
+            _image.sprite = sprite;
+            return this;
+        }
+    }
+}
