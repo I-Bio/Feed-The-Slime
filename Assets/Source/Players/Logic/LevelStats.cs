a56de8abcbe80e4c416a4e32fa1fa800e9b6897c
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-04-10 01:45:04 +0300

    Создан конфиг уровня

---
+++ Assets/Source/Players/Logic/LevelStats.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Players
+{
+    [CreateAssetMenu(fileName = "LevelConfig", menuName = "Player/LevelConfig", order = 0)]
+    public class LevelConfig : ScriptableObject
+    {
+        [SerializeField] public float ScaleFactor;
+        [SerializeField] public float CameraScale;
+        [SerializeField] public float ScoreScaler;
+        [SerializeField] public float StartScore = 0f;
+        [SerializeField] public int StartMaxScore;
+        [SerializeField] public int LevelsPerStage;
+        [SerializeField] public SatietyStage StartStage;
+    }
+}
