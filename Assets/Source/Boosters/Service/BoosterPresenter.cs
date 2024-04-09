33771bd4ee2bf955c768bb538facc5ac7a60a568
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-04-10 01:10:35 +0300

    Облегчение презентера и преоразование его в визитер

--- Assets/Source/Boosters/Service/BoosterPresenter.cs
+++ Assets/Source/Boosters/Service/BoosterPresenter.cs
@@ -1,88 +1,65 @@
 ﻿namespace Boosters
 {
-    public class BoosterPresenter : IPresenter
+    public class BoosterPresenter : IPresenter, IBoosterVisitor
     {
         private readonly ISettable _scoreHolder;
         private readonly ISettable _speedHolder;
-        private readonly BoosterInjector _injector;
-        private readonly BoosterEjector _ejector;
         private readonly BoosterService _service;
         private readonly BoosterVisualizer _visualizer;
+        private readonly BoosterEjector _ejector;
 
-        public BoosterPresenter(ISettable scoreHolder, ISettable speedHolder, BoosterInjector injector,
-            BoosterEjector ejector, BoosterService service, BoosterVisualizer visualizer)
+        public BoosterPresenter(ISettable scoreHolder, ISettable speedHolder, BoosterService service, BoosterVisualizer visualizer, BoosterEjector ejector)
         {
             _scoreHolder = scoreHolder;
             _speedHolder = speedHolder;
-            _injector = injector;
-            _ejector = ejector;
             _service = service;
             _visualizer = visualizer;
+            _ejector = ejector;
         }
 
         public void Enable()
         {
-            _service.Injected += OnGoingInject;
-            _service.Ejected += OnGoingEject;
-            
-            _injector.ScoreBoosterGained += OnScoreGained;
-            _injector.SpeedBoosterGained += OnSpeedGained;
-
-            _ejector.ScoreEnded += OnScoreReset;
-            _ejector.SpeedEnded += OnSpeedReset;
+            _service.Injected += OnBoosterAccept;
+            _service.Ejected += OnEjected;
+            _ejector.Completed += OnBoosterAccept;
             
             _visualizer.Updated += OnUpdated;
         }
 
         public void Disable()
         {
-            _service.Injected -= OnGoingInject;
-            _service.Ejected -= OnGoingEject;
-            
-            _injector.ScoreBoosterGained -= OnScoreGained;
-            _injector.SpeedBoosterGained -= OnSpeedGained;
-
-            _ejector.ScoreEnded -= OnScoreReset;
-            _ejector.SpeedEnded -= OnSpeedReset;
+            _service.Injected -= OnBoosterAccept;
+            _service.Ejected -= OnEjected;
+            _ejector.Completed -= OnBoosterAccept;
 
             _visualizer.Updated -= OnUpdated;
         }
 
-        private void OnGoingInject(IStatBuffer boost)
-        {
-            boost.Accept(_injector);
-        }
-
-        private void OnGoingEject(IStatBuffer boost)
+        private void OnEjected(IStatBuffer boost)
         {
             boost.Accept(_ejector);
         }
 
-        private void OnScoreGained(ICalculableScore calculableScore)
+        private void OnBoosterAccept(IStatBuffer boost)
         {
-            _scoreHolder.SetBoost(calculableScore);
-            _visualizer.Visit(calculableScore);
-        }
-        
-        private void OnScoreReset(ICalculableScore calculableScore)
-        {
-            _scoreHolder.SetBoost(calculableScore);
-        }
-
-        private void OnSpeedGained(IMovable movable)
-        {
-            _speedHolder.SetBoost(movable);
-            _visualizer.Visit(movable);
-        }
-        
-        private void OnSpeedReset(IMovable movable)
-        {
-            _speedHolder.SetBoost(movable);
+            boost.Accept(this);
         }
 
         private void OnUpdated(float delay)
         {
             _service.Update(delay);
+        }
+
+        public void Visit(IMovable movable)
+        {
+            _speedHolder.SetBoost(movable);
+            _visualizer.Visit(movable);
+        }
+
+        public void Visit(ICalculableScore calculableScore)
+        {
+            _scoreHolder.SetBoost(calculableScore);
+            _visualizer.Visit(calculableScore);
         }
     }
 }
