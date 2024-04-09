4c7d9bf970a183f8e0963a11560d969b1fc05884
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-04-10 01:30:22 +0300

    Скрипт локализации изменяющихся ярлыков

---
+++ Assets/Source/Global/LocalizedText.cs
@@ -0,0 +1,20 @@
+﻿using Lean.Localization;
+using UnityEngine;
+
+public class LocalizedText : LeanLocalizedBehaviour
+{
+    [SerializeField] private string _label;
+
+    public string Label => _label;
+        
+    public override void UpdateTranslation(LeanTranslation translation)
+    {
+        if (translation == null)
+            return;
+            
+        if (translation.Data is string == false)
+            return;
+            
+        _label = translation.Data as string;
+    }
+}
