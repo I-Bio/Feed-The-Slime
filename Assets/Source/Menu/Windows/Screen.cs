49bd0f957e18e0724e0f4adc0bf26a13289db0b5
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-04-10 01:39:41 +0300

    Убраны неиспользуемые методы

--- Assets/Source/Menu/Windows/Screen.cs
+++ Assets/Source/Menu/Windows/Screen.cs
@@ -15,22 +15,7 @@ namespace Menu
             _currentActiveId = id;
         }
         
-        public void HideWindow(HideOption option)
-        {
-            if (option == HideOption.Main)
-                SetLast();
-            else if (option == HideOption.HideAll)
-                Hide();
-        }
-
-        private void SetLast()
-        {
-            _windows[_currentActiveId].SetActive(false);
-            _windows[^1].SetActive(true);
-            _currentActiveId = _windows.Length - 1;
-        }
-
-        private void Hide()
+        public void Hide()
         {
             _windows[_currentActiveId].SetActive(false);
         }
