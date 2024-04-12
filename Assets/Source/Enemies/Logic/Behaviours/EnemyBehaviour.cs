2589d4797d0dfbcc55eae7c0b8b9c8ae926abb87
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-04-12 23:23:21 +0300

    Движение игрока переработано в поведение

---
+++ Assets/Source/Enemies/Logic/Behaviours/EnemyBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Enemies
+{
+    public abstract class EnemyBehaviour : MonoBehaviour
+    {
+        private float _delay;
+
+        public event Action GoingThink;
+
+        protected void Initialize(float delay)
+        {
+            _delay = delay;
+            StartCoroutine(ThinkRoutine());
+        }
+
+        public abstract void Accept(IEnemyVisitor visitor, float thinkDelay);
+
+        public abstract void InteractInClose(Vector3 position);
+        
+        public abstract void AvoidInteraction(Vector3 position, Action onAvoided);
+        
+        public abstract void CancelInteraction();
+
+        private IEnumerator ThinkRoutine()
+        {
+            bool isWorking = true;
+            var wait = new WaitForSeconds(_delay);
+
+            while (isWorking)
+            {
+                GoingThink?.Invoke();
+                yield return wait;
+            }
+        }
+    }
+}
