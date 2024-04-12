abe0c9102430869327905cb86f5709a3651a464a
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-04-12 23:29:37 +0300

    Создана подсветка врага

---
+++ Assets/Source/Enemies/Logic/EnemyHighlighter.cs
@@ -0,0 +1,29 @@
+﻿using Foods;
+using UnityEngine;
+
+namespace Enemies
+{
+    public class EnemyHighlighter : ObjectHighlighter
+    {
+        [SerializeField] private Color _color = Color.white;
+
+        private bool _didColorChange;
+        
+        public override void Select(SatietyStage playerStage)
+        {
+            Outline.OutlineWidth = SelectValue;
+            base.Select(playerStage);
+        }
+        
+        public override void SetSelection()
+        {
+            base.SetSelection();
+            
+            if (_didColorChange == true)
+                return;
+
+            _didColorChange = true;
+            Outline.OutlineColor = _color;
+        }
+    }
+}
