f473f5fb0e1e9417c36379579d7df571835e37e2
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-04-12 23:34:10 +0300

    Создана полоска накопления отравления

---
+++ Assets/Source/Players/Bars/ToxinBar.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Players
+{
+    public class ToxinBar : MonoBehaviour
+    {
+        [SerializeField] private GameObject _holder;
+        [SerializeField] private Slider _slider;
+
+        private float _maxValue;
+        private int _minValue;
+        private Coroutine _routine;
+
+        public event Action Hid;
+        
+        public void Initialize(float maxValue, int minValue)
+        {
+            _maxValue = maxValue;
+            _minValue = minValue;
+            
+            ChangeValue(_minValue);
+        }
+
+        public void ChangeValue(int value)
+        {
+            _slider.value = value / _maxValue;
+            
+            if (value > _minValue && _holder.activeSelf == false)
+                Show();
+
+            if (value <= _minValue && _holder.activeSelf == true)
+                Hide();
+        }
+
+        private void Show()
+        {
+            _holder.SetActive(true);
+        }
+
+        private void Hide()
+        {
+            _holder.SetActive(false);
+            
+            Hid?.Invoke();
+        }
+    }
+}
