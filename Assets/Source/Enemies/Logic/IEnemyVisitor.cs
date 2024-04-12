4943e1f4ad84a415209ac2cdd611703ba6bd7f55
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-04-12 23:29:20 +0300

    Создан визитер врага

---
+++ Assets/Source/Enemies/Logic/IEnemyVisitor.cs
@@ -0,0 +1,9 @@
+﻿namespace Enemies
+{
+    public interface IEnemyVisitor
+    {
+        public void Visit(EnemyMover mover, float thinkDelay);
+        public void Visit(EnemyToxin toxin, float thinkDelay);
+        public void Visit(EnemySwarm swarm, float thinkDelay);
+    }
+}
