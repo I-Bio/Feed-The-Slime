a1b10a02e186960384e0a208271a671d14ce6e0b
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-04-19 01:52:54 +0300

    Переименованы поля у InputPresenter

--- Assets/Source/Input/InputPresenter.cs
+++ Assets/Source/Input/InputPresenter.cs
@@ -6,92 +6,95 @@ namespace Input
 {
     public class InputPresenter : IPresenter
     {
-        private readonly PlayerInput _model;
-        private readonly Joystick _joystick;
-        private readonly IReadable _player;
-        private readonly ICaster _caster;
-        private readonly ILoader _loader;
+        private readonly PlayerInput Model;
+        private readonly Joystick Joystick;
+        private readonly IReadable Player;
+        private readonly ICaster Caster;
+        private readonly ILoader Loader;
 
         public InputPresenter(PlayerInput model, Joystick joystick, IReadable player, ICaster caster, ILoader loader)
         {
-            _model = model;
-            _joystick = joystick;
-            _player = player;
-            _caster = caster;
-            _loader = loader;
+            Model = model;
+            Joystick = joystick;
+            Player = player;
+            Caster = caster;
+            Loader = loader;
         }
 
         public void Enable()
         {
-            _model.Player.Move.performed += OnMoved;
-            _model.Player.Touch.performed += OnTouched;
-            _model.Player.Hide.started += OnHid;
-            _model.Player.Hide.performed += OnShowed;
-            _model.Player.Spit.started += OnCastStarted;
-            _model.Player.Spit.performed += OnCastPerformed;
-            _model.Player.Load.performed += OnLoadPerformed;
-            _joystick.Released += OnJoystickReleased;
+            Model.Player.Move.performed += OnMoved;
+            Model.Player.Touch.performed += OnTouched;
+            Model.Player.Hide.started += OnHid;
+            Model.Player.Hide.performed += OnShowed;
+            Model.Player.Spit.started += OnCastStarted;
+            Model.Player.Spit.performed += OnCastPerformed;
+            Model.Player.Load.performed += OnLoadPerformed;
+            Joystick.Released += OnJoystickReleased;
 
-            _model.Enable();
+            Model.Enable();
         }
 
         public void Disable()
         {
-            _model.Player.Move.performed -= OnMoved;
-            _model.Player.Touch.performed -= OnTouched;
-            _model.Player.Hide.started -= OnHid;
-            _model.Player.Hide.performed -= OnShowed;
-            _model.Player.Spit.started -= OnCastStarted;
-            _model.Player.Spit.performed -= OnCastPerformed;
-            _model.Player.Load.performed -= OnLoadPerformed;
-            _joystick.Released -= OnJoystickReleased;
+            Model.Player.Move.performed -= OnMoved;
+            Model.Player.Touch.performed -= OnTouched;
+            Model.Player.Hide.started -= OnHid;
+            Model.Player.Hide.performed -= OnShowed;
+            Model.Player.Spit.started -= OnCastStarted;
+            Model.Player.Spit.performed -= OnCastPerformed;
+            Model.Player.Load.performed -= OnLoadPerformed;
+            Joystick.Released -= OnJoystickReleased;
 
-            _model.Disable();
+            Model.Disable();
         }
 
         private void OnMoved(InputAction.CallbackContext context)
         {
-            _player.ReadInput(context.ReadValue<Vector2>());
+            Player.ReadInput(context.ReadValue<Vector2>());
         }
 
         private void OnTouched(InputAction.CallbackContext context)
         {
-            Vector2 position = _model.Player.ScreenPosition.ReadValue<Vector2>();
+            if (Joystick.isActiveAndEnabled == false)
+                return;
+            
+            Vector2 position = Model.Player.ScreenPosition.ReadValue<Vector2>();
             
             if (position == Vector2.zero)
                 return;
             
-            _joystick.Activate(position);
+            Joystick.Activate(position);
         }
 
         private void OnJoystickReleased()
         {
-            _player.ReadInput(Vector2.zero);
+            Player.ReadInput(Vector2.zero);
         }
 
         private void OnHid(InputAction.CallbackContext context)
         {
-            _caster.Hide();
+            Caster.Hide();
         }
         
         private void OnShowed(InputAction.CallbackContext context)
         {
-            _caster.Show();
+            Caster.Show();
         }
         
         private void OnCastStarted(InputAction.CallbackContext context)
         {
-            _caster.DrawCastTrajectory();
+            Caster.DrawCastTrajectory();
         }
         
         private void OnCastPerformed(InputAction.CallbackContext context)
         {
-            _caster.CastSpit();
+            Caster.CastSpit();
         }
 
         private void OnLoadPerformed(InputAction.CallbackContext context)
         {
-            _loader.Load();
+            Loader.Load();
         }
     }
 }
