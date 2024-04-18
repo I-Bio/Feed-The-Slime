a48009a11a45ae80d8fda4eac5b6d39d0f965885
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-04-19 01:58:29 +0300

    Создана инициализация сванеру

--- Assets/Source/Spawners/EatableSpawner.cs
+++ Assets/Source/Spawners/EatableSpawner.cs
@@ -5,6 +5,11 @@ namespace Spawners
 {
     public class EatableSpawner : ObjectPool
     {
+        public void Initialize(FoodSetup dissolved)
+        {
+            Initialize(dissolved as SpawnableObject);
+        }
+        
         public void Spawn(Vector3 position, float score)
         {
             Pull<FoodSetup>(position).Initialize(score);
