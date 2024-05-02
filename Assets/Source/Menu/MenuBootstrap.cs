bd03ec7cc255e3032cf1026672f8c8660fbae34c
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-05-02 22:08:50 +0300

    Изменен порядок инициализации загрузчика меню

--- Assets/Source/Menu/MenuBootstrap.cs
+++ Assets/Source/Menu/MenuBootstrap.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Linq;
-using Lean.Localization;
+﻿using Lean.Localization;
+using Spawners;
 using TMPro;
 using UnityEngine;
 
@@ -8,30 +7,33 @@ namespace Menu
 {
     [RequireComponent(typeof(YandexLeaderboard))]
     [RequireComponent(typeof(LeaderboardView))]
+    [RequireComponent(typeof(Stopper))]
     public class MenuBootstrap : MonoBehaviour
     {
-        [Header("Leaderboard")]
-        [SerializeField] private TextMeshProUGUI _name;
         [SerializeField] private string _leaderboardName = "Leaderboard";
         [SerializeField] private string _anonymousName = "Anonymous";
         [SerializeField] private Transform _container;
+        [SerializeField] private LeaderboardElement _template;
 
         [SerializeField] private ProgressSetup _progress;
+        [SerializeField] private LevelBootstrap _bootstrap;
 
         private YandexLeaderboard _yandexLeaderboard;
         private LeaderboardView _leaderboard;
+        private Stopper _stopper;
 
         private void Awake()
         {
             _yandexLeaderboard = GetComponent<YandexLeaderboard>();
             _leaderboard = GetComponent<LeaderboardView>();
-            _progress.Initialize(_yandexLeaderboard);
+            _stopper = GetComponent<Stopper>();
+            _progress.Initialize(_yandexLeaderboard, _bootstrap, _stopper);
         }
 
         private void Start()
         {
             _anonymousName = LeanLocalization.GetTranslationText(_anonymousName);
-            _yandexLeaderboard.Initialize(_leaderboard, _name, _leaderboardName, _anonymousName, _container);
+            _yandexLeaderboard.Initialize(_leaderboard, _leaderboardName, _anonymousName, _container, _template);
         }
     }
 }
