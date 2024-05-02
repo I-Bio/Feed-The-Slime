a7bf33e8cf09bd839aed7d769639d2c2cfc27233
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-05-02 22:08:20 +0300

    Изменение воспроизведения награды

---
+++ Assets/Source/Menu/RewardGem.cs
@@ -0,0 +1,12 @@
+﻿using Spawners;
+using UnityEngine;
+
+namespace Menu
+{
+    public class RewardGem : SpawnableObject
+    {
+        [SerializeField] private RectTransform _rectTransform;
+        
+        public RectTransform RectTransform => _rectTransform;
+    }
+}
--- Assets/Source/Menu/RewardReproducer.cs
+++ Assets/Source/Menu/RewardReproducer.cs
@@ -1,51 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
 using DG.Tweening;
+using Spawners;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Menu
 {
-    public class RewardReproducer : MonoBehaviour
+    public class RewardReproducer : ObjectPool
     {
         [SerializeField] private AudioSource _sound;
-        [SerializeField] private GameObject _holder;
-        [SerializeField] private float _durationPerPoint;
-        [SerializeField] private RectTransform[] _gems;
+        [SerializeField] private float _duration;
+        [SerializeField] private float _durationOffSet;
+        [SerializeField] private int _gemsCount;
         [SerializeField] private RectTransform _target;
+        [SerializeField] private RectTransform _creator;
         [SerializeField] private RectTransform _parent;
-        
+        [SerializeField] private float _offsetX;
+        [SerializeField] private float _offsetY;
+
+        private RewardGem[] _gems;
+        private Vector2 _size;
         private int _pointer;
-        private Action _callback;
+        private bool _isPlaying;
 
-        public void Reproduce(Action callback)
+        public void Initialize(RewardGem template)
         {
-            Vector2 position = _parent.TransformPoint(_target.localPosition);
-            Sequence sequence = DOTween.Sequence();
-            _callback = callback;
-            _holder.SetActive(true);
+            base.Initialize(template);
+            _size = template.RectTransform.sizeDelta;
+            _gems = new RewardGem[_gemsCount];
+        }
 
-            foreach (RectTransform gem in _gems)
+        public void Reproduce()
+        {
+            if (_isPlaying == true)
+                return;
+            
+            _isPlaying = true;
+            Vector3 targetPosition = _parent.TransformPoint(_target.localPosition);
+
+            for (int i = 0; i < _gemsCount; i++)
             {
-                gem.gameObject.SetActive(true);
-                
-                sequence
-                    .Append(gem.DOMove(position, _durationPerPoint).OnComplete(PlaySound))
-                    .Join(gem.DOSizeDelta(_target.sizeDelta, _durationPerPoint));
+                Vector2 position = new Vector2(Random.Range(-_offsetX, _offsetX), Random.Range(-_offsetY, _offsetY));
+                float duration = Random.Range(_duration - _durationOffSet, _duration + _durationOffSet);
+                _gems[i] = Pull<RewardGem>(_creator);
+                _gems[i].RectTransform.anchoredPosition = position;
+                _gems[i].RectTransform.DOMove(targetPosition, duration).SetUpdate(true);
+                _gems[i].RectTransform.DOSizeDelta(_target.sizeDelta, duration).SetUpdate(true).OnComplete(PlaySound);
             }
-
-            sequence.Play().OnComplete(Stop);
         }
 
         private void PlaySound()
         {
             _sound.Play();
-            _gems[_pointer].gameObject.SetActive(false);
+            _gems[_pointer].Push();
+            _gems[_pointer].RectTransform.position = Vector3.zero;
+            _gems[_pointer].RectTransform.sizeDelta = _size;
             _pointer++;
+
+            if (_pointer == _gems.Length)
+                Stop();
         }
 
         private void Stop()
         {
-            _holder.SetActive(false);
-            _callback?.Invoke();
+            _pointer = 0;
+            _isPlaying = false;
         }
     }
 }
