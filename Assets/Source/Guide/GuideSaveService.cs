e1092cc76b6a311c9408e7108e51dd041235c88f
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-05-06 00:34:17 +0300

    Сохранение громкости в гайде

---
+++ Assets/Source/Guide/GuideSaveService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Menu;
+using PlayerPrefs = Agava.YandexGames.Utility.PlayerPrefs;
+
+namespace Guide
+{
+    public class GuideSaveService : SaveService
+    {
+        public GuideSaveService(Action<IReadOnlyCharacteristics> onFinished,
+            IReadOnlyCharacteristics startCharacteristics) : base(onFinished, startCharacteristics) {}
+        
+        public void OnGameVolumeChanged(float volume)
+        {
+            PlayerPrefs.SetFloat(nameof(CharacteristicConstants.GameVolume), volume);
+        }
+        
+        public void OnMusicVolumeChanged(float volume)
+        {
+            PlayerPrefs.SetFloat(nameof(CharacteristicConstants.MusicVolume), volume);
+        }
+    }
+}
