56b78df2e94f489692bfc0a50e73f3798fe80a8f
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-05-06 00:26:00 +0300

    Изменение CanvasGroup при закрытии вынесены в отдельный метод

--- Assets/Source/Menu/Windows/Window.cs
+++ Assets/Source/Menu/Windows/Window.cs
@@ -13,10 +13,13 @@ namespace Menu
         [SerializeField] private bool _isPausing;
         [SerializeField] private bool _isReleasing;
         [SerializeField] private bool _have3dElements;
+        [SerializeField] private GameObject[] _enablingObjects;
         
         private CanvasGroup _group;
         private Stopper _stopper;
         private GameObject _gameObject;
+
+        public bool IsActive => _group.interactable;
 
         public void Initialize(Stopper stopper)
         {
@@ -35,6 +38,13 @@ namespace Menu
             if (_have3dElements == true)
                 _gameObject.SetActive(true);
             
+            foreach(GameObject enabling in _enablingObjects)
+                if (enabling != null)
+                    enabling.SetActive(true);
+            
+            if (_group == null)
+                return;
+            
             _group.alpha = OnAlpha;
             _group.interactable = true;
             _group.blocksRaycasts = true;
@@ -42,15 +52,27 @@ namespace Menu
 
         public void Close()
         {
-            _group.alpha = OffAlpha;
-            _group.interactable = false;
-            _group.blocksRaycasts = false;
+            TurnOff();
+            
+            foreach(GameObject disabling in _enablingObjects)
+                if (disabling != null)
+                    disabling.SetActive(false);
             
             if (_have3dElements == true)
                 _gameObject.SetActive(false);
             
             if (_isReleasing == true)
                 _stopper.Release();
+        }
+
+        public void TurnOff()
+        {
+            if (_group == null)
+                return;
+            
+            _group.alpha = OffAlpha;
+            _group.interactable = false;
+            _group.blocksRaycasts = false;
         }
     }
 }
