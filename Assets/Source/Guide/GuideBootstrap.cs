6baa2487631189befb7a31aef28ba16916db753e
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-05-06 00:43:03 +0300

    Добавлен звук,  заполнитель и переключатель доп окна в гайд

--- Assets/Source/Guide/GuideBootstrap.cs
+++ Assets/Source/Guide/GuideBootstrap.cs
@@ -7,10 +7,13 @@ using Menu;
 using Players;
 using Spawners;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 namespace Guide
 {
+    [RequireComponent(typeof(FadeCaster))]
+    [RequireComponent(typeof(SoundChanger))]
     public class GuideBootstrap : MonoBehaviour
     {
         [SerializeField] private BoosterSpawner _boosterSpawner;
@@ -20,12 +23,18 @@ namespace Guide
         [SerializeField] private Guide _guide;
         [SerializeField] private ThemePreparer _theme;
         
-        [Space, Header("FadeCaster")] 
+        [Space, Header(nameof(SoundChanger))]
+        [SerializeField] private AudioMixer _mixer;
+        [SerializeField] private Slider _game;
+        [SerializeField] private Slider _music;
+        [SerializeField] private AudioSource _sound;
+        
+        [Space, Header(nameof(FadeCaster))] 
         [SerializeField] private LayerMask _fadeMask;
         [SerializeField] private float _delay = 0.01f;
         [SerializeField] private int _hitsCapacity = 10;
         
-        [Space, Header("Guide")]
+        [Space, Header(nameof(Guide))]
         [SerializeField] private Button[] _nextButtons;
         [SerializeField] private Button[] _releaseButtons;
         [SerializeField] private ObjectHighlighter _exampleFood;
@@ -36,14 +45,31 @@ namespace Guide
         [SerializeField] private GuideTrigger _trigger;
         [SerializeField] private Button _close;
         [SerializeField] private Button _pause;
+        [SerializeField] private Button[] _loadButtons;
+        [SerializeField] private ObjectFiller _filler;
+
+        [Space, Header(nameof(SubWindowSwitcher))] 
+        [SerializeField] private SubWindowSwitcher _subSwitcher;
+        [SerializeField] private Button[] _subCloseButtons;
+        [SerializeField] private Button[] _subVolumeButtons;
+        [SerializeField] private Window[] _subParents;
 
         private FadeCaster _fadeCaster;
-        private SaveService _saveService;
+        private SoundChanger _changer;
+        private GuideSaveService _saveService;
         
         private void Awake()
         {
-            _saveService = new SaveService(Launch, _characteristics);
+            _saveService = new GuideSaveService(Launch, _characteristics);
             _saveService.Load();
+            _changer.GameVolumeChanged += _saveService.OnGameVolumeChanged;
+            _changer.MusicVolumeChanged += _saveService.OnMusicVolumeChanged;
+        }
+
+        private void OnDestroy()
+        {
+            _changer.GameVolumeChanged -= _saveService.OnGameVolumeChanged;
+            _changer.MusicVolumeChanged -= _saveService.OnMusicVolumeChanged;
         }
 
         private void Launch(IReadOnlyCharacteristics characteristics)
@@ -53,21 +79,26 @@ namespace Guide
                 new AdditionalScore(new Score(), characteristics.ScorePerEat, float.MinValue, null);
             IHidden hidden = _player.GetComponent<IHidden>();
             EnemyDependencyVisitor visitor = new EnemyDependencyVisitor(_player.GetComponent<IPlayerVisitor>());
+            
             _fadeCaster = GetComponent<FadeCaster>();
+            _changer = GetComponent<SoundChanger>();
 
-            _input.Initialize(_guide);
+            _input.Initialize();
             _player.Initialize(movable, calculableScore, _guide);
-            _guide.Initialize(_nextButtons, _releaseButtons, _pause);
             _boosterSpawner.Initialize(movable, calculableScore);
             _theme.Initialize(hidden, visitor);
+            _changer.Initialize(_mixer, _game, _music, _sound);
             _beacon.Initialize(_guide);
             _trigger.Initialize(_camera, _close, _player.transform, _enemy,
                 () => { _guide.ChangeWindow(GuideWindows.Enemy); },
                 () => { _guide.Release(); });
-            _exampleFood.Initialize((float)CharacteristicConstants.Zero, _selectValue);
+            _exampleFood.Initialize((float)ValueConstants.Zero, _selectValue);
             _fadeCaster.Initialize(_fadeMask, _player.transform, _camera.transform, _delay, _hitsCapacity);
-
+            _filler.Initialize();
             _exampleFood.SetSelection();
+            _changer.Load((float)ValueConstants.Zero, (float)ValueConstants.Zero);
+            _guide.Initialize(_nextButtons, _releaseButtons, _loadButtons, _pause, _filler,
+                _subSwitcher, _subCloseButtons, _subVolumeButtons, _subParents);
         }
     }
 }
