e41c03930982cd0183ec95410284961d66f62410
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-05-06 00:24:47 +0300

    Создан класс доп окон

---
+++ Assets/Source/Menu/Windows/SubWindowSwitcher.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Menu
+{
+    [RequireComponent(typeof(Screen))]
+    public class SubWindowSwitcher : MonoBehaviour
+    {
+        private Screen _screen;
+        private Button[] _closeButtons;
+        private Button[] _volumeButtons;
+        private Window[] _parents;
+        private List<Window> _toOpen;
+
+        private void OnDestroy()
+        {
+            foreach (Button close in _closeButtons)
+                close.onClick.RemoveListener(Hide);
+            
+            foreach (Button volume in _volumeButtons)
+                volume.onClick.RemoveListener(ShowVolume);
+        }
+
+        public void Initialize(Stopper stopper, Button[] closeButtons, Button[] volumeButtons, Window[] parents)
+        {
+            _closeButtons = closeButtons;
+            _volumeButtons = volumeButtons;
+            _parents = parents;
+            _toOpen = new List<Window>();
+            _screen = GetComponent<Screen>();
+            _screen.Initialize(stopper);
+
+            foreach (Button close in _closeButtons)
+                close.onClick.AddListener(Hide);
+            
+            foreach (Button volume in _volumeButtons)
+                volume.onClick.AddListener(ShowVolume);
+        }
+
+        private void ChangeWindow(SubWindows window)
+        {
+            _screen.SetWindow((int)window);
+            
+            foreach (Window parent in _parents)
+            {
+                if (parent.IsActive == false)
+                    continue;
+                
+                parent.TurnOff();
+                _toOpen.Add(parent);
+            }
+        }
+
+        private void Hide()
+        {
+            foreach (Window parent in _toOpen)
+                parent.Open();
+
+            _screen.Hide();
+            _toOpen.Clear();
+        }
+        
+        private void ShowVolume()
+        {
+            ChangeWindow(SubWindows.Pause);
+        }
+    }
+}
---
+++ Assets/Source/Menu/Windows/SubWindows.cs
@@ -0,0 +1,7 @@
+﻿namespace Menu
+{
+    public enum SubWindows
+    {
+        Pause
+    }
+}
