3bdb5a760718eb11e659780226013d9faef2e04a
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-05-09 14:48:00 +0300

    Убраны всплывающие цифры из бара

--- Assets/Source/Players/Bars/LevelBar.cs
+++ Assets/Source/Players/Bars/LevelBar.cs
@@ -1,46 +1,35 @@
-﻿using Spawners;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Players
 {
     [RequireComponent(typeof(LocalizedText))]
-    public class LevelBar : ObjectPool
+    public class LevelBar : MonoBehaviour
     {
         [SerializeField] private Slider _slider;
-        [SerializeField] private RectTransform _targetPoint;
-        [SerializeField] private Transform _container;
         [SerializeField] private string _slash;
         [SerializeField] private TextMeshProUGUI _score;
         [SerializeField] private TextMeshProUGUI _level;
 
         private LocalizedText _localized;
-        private Vector2 _targetPosition;
         
         public void Initialize(float score, int maxScore)
         {
             _localized = GetComponent<LocalizedText>();
             ChangeScore(score, maxScore);
             SetLevel((int)score);
-            _targetPosition = _targetPoint.anchoredPosition;
         }
         
-        public void SetScore(float score, int maxScore, float value)
-        {
-            Pull<PopUpText>(_container).Initialize(value, _targetPosition);
-            ChangeScore(score, maxScore);
-        }
-
         public void SetLevel(int levelPoint)
         {
             _level.SetText($"{_localized.Label} {levelPoint}");
         }
 
-        private void ChangeScore(float score, int maxScore)
+        public void ChangeScore(float score, int maxScore)
         {
             _slider.value = score / maxScore;
-            _score.SetText($"{Mathf.FloorToInt(score)}{_slash}{maxScore}");
+            _score.SetText($"{score:0.0}{_slash}{maxScore:0.0}");
         }
     }
 }
