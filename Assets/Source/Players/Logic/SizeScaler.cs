2ab597a6cc3139fcd60b66501cac65354dc5eacf
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-05-29 01:46:32 +0300

    Добавлена возможность обратного вызова после увеличения в размерах
    вследствие чего исправлен эффект стадии, его движение за игроком

--- Assets/Source/Players/Logic/SizeScaler.cs
+++ Assets/Source/Players/Logic/SizeScaler.cs
@@ -1,31 +1,39 @@
-﻿using Cinemachine;
+﻿using System;
+using Cinemachine;
 using UnityEngine;
 
 namespace Players
 {
     public class SizeScaler : MonoBehaviour
     {
-        private Transform _transform;
+        private Transform _player;
+        private Transform _camera;
         private CinemachineTransposer _virtualCamera;
         private float _scaleFactor;
         private float _cameraScale;
         private Vector3 _startScale;
         private Vector3 _startOffset;
 
-        public void Initialize(Transform transform, CinemachineVirtualCamera virtualCamera, float scaleFactor, float cameraScale)
+        public void Initialize(Transform player, CinemachineVirtualCamera virtualCamera, float scaleFactor, float cameraScale)
         {
-            _transform = transform;
+            _player = player;
+            _camera = virtualCamera.transform;
             _virtualCamera = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
             _scaleFactor = scaleFactor;
             _cameraScale = cameraScale;
-            _startScale = _transform.localScale;
+            _startScale = _player.localScale;
             _startOffset = _virtualCamera.m_FollowOffset;
         }
 
-        public void Scale(SatietyStage stage)
+        public void Scale(SatietyStage stage, Action<Vector3> onScaledCallback = null)
         {
-            _transform.localScale = _startScale * (_scaleFactor * (int)stage);
-            _virtualCamera.m_FollowOffset = _startOffset * (_cameraScale * (int)stage);
+            if (stage == SatietyStage.Exhaustion)
+                return;
+            
+            _player.localScale = _startScale * (_scaleFactor * (int)stage);
+            Vector3 newOffset = _startOffset * (_cameraScale * (int)stage);
+            onScaledCallback?.Invoke(_camera.position - _virtualCamera.m_FollowOffset + newOffset);
+            _virtualCamera.m_FollowOffset = newOffset;
         }
     }
 }
