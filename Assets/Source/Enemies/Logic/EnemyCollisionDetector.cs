67f756d0503915de12c6e6145745572f58f5849f
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-05-29 01:29:21 +0300

    Детектор колизий врага изменил логику

--- Assets/Source/Enemies/Logic/EnemyCollisionDetector.cs
+++ Assets/Source/Enemies/Logic/EnemyCollisionDetector.cs
@@ -5,25 +5,15 @@ namespace Enemies
 {
     public class EnemyCollisionDetector : MonoBehaviour
     {
-        private bool _canContact;
         private SatietyStage _stage;
 
         public void Initialize(SatietyStage stage)
         {
-            _canContact = true;
             _stage = stage;
         }
-        
-        public void DisallowContact()
-        {
-            _canContact = false;
-        }
-        
+
         private void OnCollisionEnter(Collision collision)
         {
-            if (_canContact == false)
-                return;
-
             if (collision.collider.TryGetComponent(out IPlayerVisitor player) == false)
                 return;
 
