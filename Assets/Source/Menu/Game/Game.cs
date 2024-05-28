a4cc2d71701bd70a0b6f9348fe8d021c52a1f934
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-05-29 01:43:23 +0300

    Исправлен вызов рекламы

--- Assets/Source/Menu/Game/Game.cs
+++ Assets/Source/Menu/Game/Game.cs
@@ -18,8 +18,9 @@ namespace Menu
 
         private WindowSwitcher _switcher;
         private Stopper _stopper;
+        private AutoSaveRequester _requester;
         private Revival _revival;
-        private int _stage;
+        private SatietyStage _stage;
         private int _rewardCount;
         private float _maxStage;
         private float _stageScale;
@@ -36,14 +37,15 @@ namespace Menu
                 load.onClick.RemoveListener(Load);
         }
 
-        public void Initialize(Revival revival, WindowSwitcher switcher, Stopper stopper, int reward)
+        public void Initialize(Revival revival, WindowSwitcher switcher, Stopper stopper, AutoSaveRequester requester, int rewardCount)
         {
             _revival = revival;
-            _rewardCount = reward;
+            _rewardCount = rewardCount;
             _maxStage = Enum.GetValues(typeof(SatietyStage)).Length - 1;
             SetStage(SatietyStage.Exhaustion);
             _switcher = switcher;
             _stopper = stopper;
+            _requester = requester;
 
             _winAdvert.onClick.AddListener(ShowWinAdvert);
             _loseAdvert.onClick.AddListener(ShowLoseAdvert);
@@ -52,23 +54,30 @@ namespace Menu
                 load.onClick.AddListener(Load);
 
             _switcher.ChangeWindow(Windows.Play);
+            _requester.StartRequests();
         }
 
         public void SetStage(SatietyStage stage)
         {
-            _stage = (int)stage;
+            _stage = stage;
             UpdateReward();
         }
 
         public void Win()
         {
             _didPass = true;
+            _requester.StopRequests();
             _switcher.ChangeWindow(Windows.Win);
         }
 
         public void Lose()
         {
-            _switcher.ChangeWindow(Windows.Lose, _revival);
+            _requester.StopRequests();
+            
+            if (_revival.TryRevive() == false)
+                _switcher.ChangeWindow(Windows.Lose);
+            
+            _revival.Revive();
         }
 
         public void Load()
@@ -79,16 +88,27 @@ namespace Menu
 
         private void ShowWinAdvert()
         {
-            ShowRewardAdvert(DoubleReward, _stopper.FocusRelease);
+            ShowRewardAdvert(
+                onReward: () =>
+                {
+                    _winAdvert.gameObject.SetActive(false);
+                    _rewardCount = Mathf.CeilToInt(_rewardCount * _double);
+                    UpdateReward();
+                },
+                onClose: () => _stopper.FocusRelease(true));
         }
 
         private void ShowLoseAdvert()
         {
-            ShowRewardAdvert(Respawn, () =>
-            {
-                _switcher.ResumeScreen();
-                _stopper.FocusRelease();
-            });
+            ShowRewardAdvert(
+                onReward: () => _loseAdvert.gameObject.SetActive(false),
+                onClose:
+                () =>
+                {
+                    _switcher.ResumeScreen();
+                    _requester.StartRequests();
+                    _stopper.FocusRelease(true);
+                });
         }
 
         private void ShowRewardAdvert(Action onReward, Action onClose)
@@ -98,27 +118,16 @@ namespace Menu
             onClose?.Invoke();
 #endif
 #if UNITY_WEBGL && !UNITY_EDITOR
-            VideoAd.Show(_stopper.FocusPause, onReward, onClose);
+            VideoAd.Show((() => _stopper.FocusPause(true)), onReward, onClose);
 #endif
-        }
-
-        private void Respawn()
-        {
-            _loseAdvert.gameObject.SetActive(false);
-            _revival.Revive();
-        }
-
-        private void DoubleReward()
-        {
-            _winAdvert.gameObject.SetActive(false);
-            _rewardCount = Mathf.CeilToInt(_rewardCount * _double);
-            UpdateReward();
         }
 
         private void UpdateReward()
         {
-            _stageScale = _stage == (int)SatietyStage.Exhaustion ? _minPercent : _stage / _maxStage;
-
+            _stageScale = _stage == SatietyStage.Exhaustion ? _minPercent : (int)_stage / _maxStage;
+            Debug.Log($"UPDATE REWARD\n VARIABLES:  Stage: {_stage}, StageScale_Current: {_stageScale}, " +
+                      $"stageScale_Mean: {(int)_stage / _maxStage}, Reward_Mean: {_rewardCount}, " +
+                      $"Reward_Current: {_rewardCount * _stageScale} & ceil {Mathf.CeilToInt(_rewardCount * _stageScale)}");
             foreach (TextMeshProUGUI reward in _rewards)
                 reward.SetText(Mathf.CeilToInt(_rewardCount * _stageScale).ToString());
         }
