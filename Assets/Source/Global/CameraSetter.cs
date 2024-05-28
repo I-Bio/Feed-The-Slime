4623be2f0a45091c3d29397290b6380b05d6defb
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-05-29 00:53:32 +0300

    Убран лишний неймспейс

---
+++ Assets/Source/Global/CameraSetter.cs
@@ -0,0 +1,19 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class CameraSetter : MonoBehaviour
+{
+    private CinemachineTransposer _virtualCamera;
+    private Vector3 _position;
+
+    public void Initialize(CinemachineVirtualCamera virtualCamera, Vector3 position)
+    {
+        _virtualCamera = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        _position = position;
+    }
+
+    public void ChangeToGameView()
+    {
+        _virtualCamera.m_FollowOffset = _position;
+    }
+}
