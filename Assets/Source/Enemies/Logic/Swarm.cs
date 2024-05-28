ed6d829268a20539d55bdc8ff542fa44984a3f64
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-05-29 01:28:26 +0300

    Переработан рой

---
+++ Assets/Source/Enemies/Logic/Swarm.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Enemies
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class Swarm : MonoBehaviour
+    {
+        private NavMeshAgent _agent;
+        
+        public void Initialize(float delay)
+        {
+            _agent = GetComponent<NavMeshAgent>();
+        }
+
+        public void Move(Vector3 position)
+        {
+            _agent.SetDestination(position);
+        }
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
