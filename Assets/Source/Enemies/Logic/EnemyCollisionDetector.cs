0eba820ed6af30dd935d79d9f0a170b928bac001
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-06-03 22:55:52 +0300

    Переработан детектор колизий врага

--- Assets/Source/Enemies/Logic/EnemyCollisionDetector.cs
+++ Assets/Source/Enemies/Logic/EnemyCollisionDetector.cs
@@ -3,21 +3,27 @@ using UnityEngine;
 
 namespace Enemies
 {
-    public class EnemyCollisionDetector : MonoBehaviour
+    [RequireComponent(typeof(Collider))]
+    public class EnemyCollisionDetector : Contactable
     {
         private SatietyStage _stage;
+        private IPlayerVisitor _player;
+        private Collider _collider;
 
-        public void Initialize(SatietyStage stage)
+        public void Initialize(SatietyStage stage, IPlayerVisitor player)
         {
             _stage = stage;
+            _player = player;
+            _collider = GetComponent<Collider>();
         }
 
-        private void OnCollisionEnter(Collision collision)
+        public override bool TryContact(Bounds bounds)
         {
-            if (collision.collider.TryGetComponent(out IPlayerVisitor player) == false)
-                return;
-
-            player.Visit(null, _stage);
+            if (bounds.Intersects(_collider.bounds) == false)
+                return false;
+            
+            _player.Visit(null, _stage);
+            return true;
         }
     }
 }
