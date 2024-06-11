b80000222c6d942fafcbd5c0e58fbcf67b5495c8
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-06-11 15:30:16 +0300

    Добавлена фабрика тем

---
+++ Assets/Source/Menu/Game/IThemeFactory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Menu
+{
+    public interface IThemeFactory
+    {
+        public List<Contactable> CreateCenter(Transform point, out List<ISelectable> selectables);
+        public List<Contactable> CreateTheme(Transform point, out List<ISelectable> selectables);
+    }
+}
---
+++ Assets/Source/Menu/Game/ThemeFactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Players;
+using Spawners;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Menu
+{
+    public class ThemeFactory : IThemeFactory
+    {
+        private readonly SerializedPair<int, ThemePreparer[]>[] ZoneTemplates;
+        private readonly SerializedPair<int, CenterPreparer[]>[] CenterTemplates;
+        private readonly Renderer Ground;
+        private readonly IHidden Hidden;
+        private readonly IPlayerVisitor Visitor;
+        private readonly int DifficultId;
+        private readonly Action<AudioSource> OnAudioFoundCallback;
+        
+        public ThemeFactory(SerializedPair<int, ThemePreparer[]>[] zoneTemplates,
+            SerializedPair<int, CenterPreparer[]>[] centerTemplates, Renderer ground, IHidden hidden,
+            IPlayerVisitor visitor, int completedLevels, Action<AudioSource> onAudioFoundCallback)
+        {
+            ZoneTemplates = zoneTemplates;
+            CenterTemplates = centerTemplates;
+            Ground = ground;
+            Hidden = hidden;
+            Visitor = visitor;
+            OnAudioFoundCallback = onAudioFoundCallback;
+            DifficultId = 0;
+            
+            for (int i = 0; i < ZoneTemplates.Length; i++)
+            {
+                if (ZoneTemplates[i].Key > completedLevels)
+                    break;
+
+                DifficultId = i;
+            }
+        }
+        
+        public List<Contactable> CreateCenter(Transform point, out List<ISelectable> selectables)
+        {
+            return Object.Instantiate(CenterTemplates[DifficultId].Value.GetRandom(), point)
+                .Initialize(Hidden, Visitor, out selectables, Ground);
+        }
+
+        public List<Contactable> CreateTheme(Transform point, out List<ISelectable> selectables)
+        {
+            return Object.Instantiate(ZoneTemplates[DifficultId].Value.GetRandom(), point)
+                .Initialize(Hidden, Visitor, out selectables, OnAudioFoundCallback);
+        }
+    }
+}
