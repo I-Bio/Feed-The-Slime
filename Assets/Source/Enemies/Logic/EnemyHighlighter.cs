7474ddb0d60da96e3d09fc841456c2cc0113b26c
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-06-19 19:47:49 +0300

    Update EnemyHighlighter.cs

--- Assets/Source/Enemies/Logic/EnemyHighlighter.cs
+++ Assets/Source/Enemies/Logic/EnemyHighlighter.cs
@@ -6,21 +6,21 @@ namespace Enemies
     public class EnemyHighlighter : ObjectHighlighter
     {
         private readonly Color Color = Color.white;
-        
+
         private bool _didColorChange;
-        
+
         public override void OnSelecting()
         {
             Highlight();
         }
-        
+
         public override void OnSetSelection()
         {
             if (_didColorChange == true)
                 return;
 
             _didColorChange = true;
-            Outline.OutlineColor = Color;
+            SetColor(Color);
         }
     }
 }
