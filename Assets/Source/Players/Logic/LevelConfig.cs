99fb970ed29ccdb1ea26af62d7c7e4fb76b2fbb4
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-06-19 19:43:39 +0300

    перенос в иерархии

---
+++ Assets/Source/Players/Logic/LevelConfig.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Players
+{
+    [CreateAssetMenu(fileName = "LevelConfig", menuName = "Player/LevelConfig", order = 0)]
+    public class LevelConfig : ScriptableObject
+    {
+        [SerializeField] private float _scaleFactor = 2.5f;
+        [SerializeField] private float _cameraScale = 2f;
+        [SerializeField] private float _scoreScaler = 2f;
+        [SerializeField] private float _startScore = 0f;
+        [SerializeField] private int _startMaxScore = 6;
+        [SerializeField] private int _levelsPerStage = 3;
+        [SerializeField] private SatietyStage _startStage = SatietyStage.Exhaustion;
+        [SerializeField] private int _minToxinsCount = 0;
+        [SerializeField] private int _maxToxinsCount = 100;
+
+        public float ScaleFactor => _scaleFactor;
+
+        public float CameraScale => _cameraScale;
+
+        public float ScoreScaler => _scoreScaler;
+
+        public float StartScore => _startScore;
+
+        public int StartMaxScore => _startMaxScore;
+
+        public int LevelsPerStage => _levelsPerStage;
+
+        public SatietyStage StartStage => _startStage;
+
+        public int MinToxinsCount => _minToxinsCount;
+
+        public int MaxToxinsCount => _maxToxinsCount;
+    }
+}
